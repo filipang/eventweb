84bc85ccc765fbeebd8b116625036ada66239376
filipang <33981940+filipang@users.noreply.github.com>
2019-11-01 20:18:43 +0200

    -working proto

--- public/js/auth.js
+++ public/js/auth.js
@@ -61,6 +61,99 @@ loginForm.addEventListener('submit', (e) => {
 
 });
 
+function removeListeners(element) {
+    var new_element = element.cloneNode(true);
+    element.parentNode.replaceChild(new_element, element);
+    return new_element;
+}
+
+function enableInterestedButton(button, uid, pid) {
+    button.style.borderStyle = "inset";
+    console.log("ENABLED INTERESTED FOR " + pid);
+    console.log(button.parentNode);
+    button = removeListeners(button);
+    button.addEventListener("click", function () {
+        firebase.database().ref().child("Users").child(uid).child("interested_posts_id_list").child(pid).remove();
+        firebase.database().ref().child("posts").child(pid).child("interested").child(uid).remove();
+        firebase.database().ref().child("posts").child(pid).child("interestedInNumber").transaction(function (currentCount) {
+            //button.textContent = (currentCount || 0) + 1;
+            return (currentCount || 0) - 1;
+        }).then(function (success) {
+            console.log(success);
+            console.log(button);
+
+            button.textContent = "INTERESTED: " + success.snapshot.val();
+            disableInterestedButton(button, uid, pid);
+        });
+    });
+}
+
+
+
+function disableInterestedButton(button, uid, pid) {
+    button.style.borderStyle = "outset";
+    console.log("DISABLED INTERESTED" + pid);
+    console.log(button.parentNode);
+    button = removeListeners(button);
+    button.addEventListener("click", function () {
+        firebase.database().ref().child("Users").child(uid).child("interested_posts_id_list").child(pid).set(true);
+        firebase.database().ref().child("posts").child(pid).child("interested").child(uid).set(true);
+        firebase.database().ref().child("posts").child(pid).child("interestedInNumber").transaction(function (currentCount) {
+            //button.textContent = (currentCount || 0) + 1;
+            return (currentCount || 0) + 1;
+        }).then(function (success) {
+            console.log(success);
+            console.log(button);
+
+            button.textContent = "INTERESTED: " + success.snapshot.val();
+            enableInterestedButton(button, uid, pid);
+        });
+  
+    });
+}
+
+function enableGoingButton(button, uid, pid) {
+    console.log("ENABLED GOING" + pid);
+    console.log(button.parentNode);
+    button = removeListeners(button);
+    button.style.borderStyle = "inset";
+    button.addEventListener("click", function () {
+        firebase.database().ref().child("Users").child(uid).child("going_posts_id_list").child(pid).remove();
+        firebase.database().ref().child("posts").child(pid).child("going").child(uid).remove();
+        firebase.database().ref().child("posts").child(pid).child("goingToNumber").transaction(function (currentCount) {
+            //button.textContent = (currentCount || 0) + 1;
+            return (currentCount || 0) - 1;
+        }).then(function (success) {
+            console.log(success);
+            console.log(button);
+
+            button.textContent = "GOING: " + success.snapshot.val();
+            disableGoingButton(button, uid, pid);
+        });
+    });
+}
+
+function disableGoingButton(button, uid, pid) {
+    console.log("DISABLED GOING" + pid);
+    console.log(button.parentNode);
+    button = removeListeners(button);
+    button.style.borderStyle = "outset";
+    button.addEventListener("click", function () {
+        firebase.database().ref().child("Users").child(uid).child("going_posts_id_list").child(pid).set(true);
+        firebase.database().ref().child("posts").child(pid).child("going").child(uid).set(true);
+        firebase.database().ref().child("posts").child(pid).child("goingToNumber").transaction(function (currentCount) {
+            //button.textContent = (currentCount || 0) + 1;
+            return (currentCount || 0) + 1;
+        }).then(function (success) {
+            console.log(success);
+            console.log(button);
+            button.textContent = "GOING: " + success.snapshot.val();
+            enableGoingButton(button, uid, pid);
+        });
+    });
+}
+
+const accountDetails = document.querySelector('.account-details');
 
 document.addEventListener('DOMContentLoaded', function () {
     var modals = document.querySelectorAll('.modal');
@@ -70,6 +163,12 @@ document.addEventListener('DOMContentLoaded', function () {
     M.Collapsible.init(items);
     auth.onAuthStateChanged(user => {
         if (user) {
+
+            const html = `
+              <div>Logged in as ${user.email}</div>
+            `;
+            accountDetails.innerHTML = html;
+
             console.log('user logged in: ', user);
             document.querySelectorAll(".logged-in").forEach(function (element) {
                 console.log("TRIED STH ON ELEMENT");
@@ -99,6 +198,14 @@ document.addEventListener('DOMContentLoaded', function () {
                     var cloned = post_template.cloneNode(true);
                     postChildKeyList.forEach(function (key) {
                         if (cloned.content.getElementById(postChildKeyToId(key)) !== null) {
+                            if (key == "interestedInNumber") {
+                                cloned.content.getElementById(postChildKeyToId(key)).textContent = "INTERESTED: " + dataSnapshot.child(key).val();
+                                return;
+                            }
+                            if(key == "goingToNumber") {
+                                cloned.content.getElementById(postChildKeyToId(key)).textContent = "GOING: " + dataSnapshot.child(key).val();
+                                return;
+                            }
                             cloned.content.getElementById(postChildKeyToId(key)).textContent = dataSnapshot.child(key).val();
                             if (key == "title") {
                                 cloned.content.getElementById("revealTitle").childNodes[0].nodeValue = dataSnapshot.child(key).val();
@@ -107,10 +214,37 @@ document.addEventListener('DOMContentLoaded', function () {
                         else console.log("ERROR ON :" + key);
                     });
                     matchPercentage = String(dataSnapshot.child("user_coef_list").child(uid).val());
-                    cloned.content.getElementById("matchPercentage").textContent = Number(matchPercentage.substring(0, 4)) * 100 + "% match";
-                    console.log(Number(matchPercentage.substring(0, 4)) * 100 + "% match");
+                    var matchnum;
+                    if (matchPercentage.length >= 5) {
+                        matchnum = Number(matchPercentage.substring(0, 4)) * 100;
+                    }
+                    else {
+                        matchnum = Number(matchPercentage) * 100;
+                    }
+
+                    console.log("FOR USER: " + uid);
+                    console.log(dataSnapshot);
+                    console.log(matchPercentage);
+                    console.log(matchnum);
+                    cloned.content.getElementById("matchPercentage").textContent = matchnum + "% match";
                     cloned.content.getElementById("imager").src = url;
+                    if (dataSnapshot.child("interested").hasChild(uid)) {
+                        enableInterestedButton(cloned.content.getElementById("interestedInNumber"), uid, dataSnapshot.key);
+                    }
+                    else {
+                        disableInterestedButton(cloned.content.getElementById("interestedInNumber"), uid, dataSnapshot.key);
+                    }
+
+                    if (dataSnapshot.child("going").hasChild(uid)) {
+                        enableGoingButton(cloned.content.getElementById("goingToNumber"), uid, dataSnapshot.key);
+                    }
+                    else {
+                        disableGoingButton(cloned.content.getElementById("goingToNumber"), uid, dataSnapshot.key);
+
+                    }
+
                     feed.appendChild(cloned.content);
+                    
                 });
 
             });
